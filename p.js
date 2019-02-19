109e838000c1ae56a4245795143793109bc8e77d
bdcostin <bailey.costin@gmail.com>
2019-02-19 13:04:13 -0500

    portfolio

---
+++ p.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+  
+//     $(window).scroll(function () {
+//         //if you hard code, then use console
+//         //.log to determine when you want the 
+//         //nav bar to stick.  
+//         console.log($(window).scrollTop())
+//       if ($(window).scrollTop() > 280) {
+//         $('#nav_bar').addClass('navbar-fixed');
+//       }
+//       if ($(window).scrollTop() < 281) {
+//         $('#nav_bar').removeClass('navbar-fixed');
+//       }
+//     });
+//   });
+
+
+// $(window).scroll(function(){
+//     $("#theFixed").css("top", Math.max(0, 100 - $(this).scrollTop()));
+// });
